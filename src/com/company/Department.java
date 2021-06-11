623d369b450d51fa26206b309a993bf83f991759
jrwaheed <j.r.waheed@gmail.com>
2021-06-11 21:09:24 +0200

    Exceptions

--- src/com/company/Department.java
+++ src/com/company/Department.java
@@ -1,6 +1,5 @@
 package com.company;
 
-
 public class Department {
     String name = "";
     Department [] subDepartments;
--- src/com/company/TooManyDepartmentsException.java
+++ src/com/company/TooManyDepartmentsException.java
@@ -1,6 +1,5 @@
 package com.company;
 
-
 public class TooManyDepartmentsException extends Exception{
     public TooManyDepartmentsException(String message){
         super(message);
